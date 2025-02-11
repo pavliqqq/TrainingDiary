ed434675b0ffdfc45e9c23074097e020e0c60a12
pavliqqq <pavlo.tiutiunnyk@nure.ua>
2025-02-11 07:55:16 +0200

    Added models Exercise and Workout, controllers and routes

---
+++ app/Http/Controllers/exercise/ExerciseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\exercise;
+
+class ExerciseController
+{
+    public function index()
+    {
+        return "Список упражнений";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\workout\WorkoutController;
+use App\Http\Controllers\exercise\ExerciseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/workouts', [WorkoutController::class, 'index']);
+Route::get('/exercises', [ExerciseController::class, 'index']);
+
